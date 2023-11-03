71b9da7cecc88111a6d0e22269d840c1b927a7bb
eriquano <mouano2@gmail.com>
2023-11-03 18:06:54 -0500

    gazebo

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/eric/TAR-Group3-Drone/catkin_ws/src
+# Build directory: /home/eric/TAR-Group3-Drone/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
